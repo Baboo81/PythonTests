81e1df694cfdd129829e6155ff04e83b17bc2f90
Baboo81 <magicbaboo@gmail.com>
2024-02-23 14:53:00 +0100

    Cls

--- main.py
+++
@@ -1,6 +0,0 @@
-nom = input("Quel est votre nom ?")
-prenom = input("Quel est votre prenom ?")
-
-nom_complet = prenom + " " + nom
-
-print(f"Bonjour {nom_complet} je suis votre assistant virtuel !")
